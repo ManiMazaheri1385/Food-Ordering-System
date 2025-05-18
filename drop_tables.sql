13781e3555fa8f05d9dfa34df3df5cde17b1d0a1
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-05-19 01:05:56 +0330

    drop tables

---
+++ drop_tables.sql
@@ -0,0 +1,6 @@
+drop table if exists Orders;
+drop table if exists Menus;
+drop table if exists Foods;
+drop table if exists Professors;
+drop table if exists Students;
+drop table if exists Persons;
